98f3ca52cb6a52a6805febd809118af50edddd57
정유리 <2024184029@tukorea.ac.kr>
2025-11-10 17:57:59 +0900

    zombie 상하좌우 방향에 맞게 시트 설정

--- zombie.py
+++ zombie.py
@@ -1,28 +1,63 @@
-from pico2d import load_image
+from pico2d import load_image, get_time
 import game_world
 from state_machine import StateMachine
 import random
-from random import randint
+from random import randint, choice
+
+# 전역 변수
+ROW_DOWN  = 0   # 정면 - 아래
+ROW_LEFT  = 1   # 왼쪽
+ROW_RIGHT = 2   # 오른쪽
+ROW_UP    = 3   # 등 - 위
+
+FRAME_COUNT = 4
+SPEED = 0.2
 
 class Zombie:
-    FRAME_COLS = 4
-    FRAME_ROWS = 4
-    SPEED = 0.03
+    global ROW_DOWN, ROW_LEFT, ROW_RIGHT, ROW_UP
+    global FRAME_COUNT, SPEED
 
     def __init__(self):
         self.image = load_image('zombie.png')
         self.frame = 0
-        self.x, self.y = randint(500, 500), randint(100, 500)
+        self.x, self.y = randint(100, 1000), randint(100, 1000)
+        dx, dy = choice([(1, 0), (-1, 0), (0, 1), (0, -1)]) # 시작 4방향 중 하나 (오, 왼, 위, 아래)
+        self.next_turn = get_time() + 1.5 #1.5초마다 자동 방향 전환
+        self.row = ROW_DOWN # 시작은 정면 방향
+        self.vx, self.vy = dx * SPEED, dy * SPEED
+        self.cols, self.rows = 4, 4
+
 
     def enter(self):
         pass
 
     def update(self):
-        self.x += self.SPEED
-        self.frame = (self.frame + 1) % (Zombie.FRAME_COLS * Zombie.FRAME_ROWS)
+        # 위치 갱신
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x < 50 or self.x > 1000:   # 왼/오른쪽 벽
+            self.vx = -self.vx            # 방향 반전
+        if self.y < 50 or self.y > 1000:   # 위/아래 벽
+            self.vy = -self.vy            # 방향 반전
+
+        t = get_time()
+        if t >= self.next_turn:
+            dx, dy = choice([(1,0), (-1,0), (0,1), (0,-1)])
+            self.vx, self.vy = dx * SPEED, dy * SPEED
+            self.next_turn = t + 1.5
+
+        # 방향으로 애니메이션 행 선택
+        if abs(self.vx) >= abs(self.vy):
+            self.row = ROW_LEFT if self.vx > 0 else ROW_RIGHT
+        else:
+            self.row = ROW_DOWN if self.vy > 0 else ROW_UP
+
+        self.frame = (self.frame + 1) % FRAME_COUNT
 
     def draw(self):
-        col = self.frame % Zombie.FRAME_COLS
-        row = self.frame // Zombie.FRAME_COLS
-        self.image.clip_draw(col * 100, row * 200, 100, 200, self.x, self.y)
+        sx = (self.frame % FRAME_COUNT) * 100
+        # sy = (self.frame // FRAME_COUNT) * 200
+        sy = self.row * 200
+        self.image.clip_draw(sx, sy, 100, 200, self.x, self.y)
 
