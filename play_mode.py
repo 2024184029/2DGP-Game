3f2655ad7cc0bb9773822bee5381991ee9d1bc6d
정유리 <2024184029@tukorea.ac.kr>
2025-11-17 15:50:36 +0900

    플레이 화면 상단에 타이머 추가

--- play_mode.py
+++ play_mode.py
@@ -6,9 +6,15 @@ from background import Background
 import game_world
 from zombie import Zombie
 from enemies import Corn, Snail, Bug
+import time
 
 running = True
 image = None
+
+start_time = 0
+elapsed_time = 0
+game_over = False
+font = None # 시간 출력 폰트
 
 def pause():
     pass
@@ -41,18 +47,46 @@ def init():
     game_world.add_object(zombie, 1)
 
     global enemies
-    enemies = [ Corn(300, 300), Snail(500, 500), Bug(700, 700) ]
+    enemies = [ Corn(300, 830), Snail(100, 350), Bug(750, 230) ]
     for e in enemies:
         game_world.add_object(e, 1)
+
+    global start_time, font, game_over
+    start_time = time.time()
+    font = load_font('D2Coding.ttc', 30)
+    game_over = False
 
 
 def update():
     game_world.update()
 
+    global start_time, elapsed_time, game_over
+
+    if game_over:
+        return
+
+    elapsed_time = time.time() - start_time
+
+    # 2분 경과 시 게임 오버
+    if elapsed_time >= 120:
+        game_over = True
+
+
+def draw_timer():
+    global elapsed_time, font
+
+    total_seconds = int(elapsed_time)
+    minutes = total_seconds // 60
+    seconds = total_seconds % 60
+    time_text = f"{minutes:02}:{seconds:02}"
+
+    font.draw(512, 900, time_text, (255, 255, 255))
+
 
 def draw():
     clear_canvas()
     game_world.render()
+    draw_timer()
     update_canvas()
 
 def finish():
