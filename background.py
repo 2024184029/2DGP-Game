8865ca985d542b065bbaa21e8b5fb9ede3d0d407
정유리 <2024184029@tukorea.ac.kr>
2025-12-06 23:27:14 +0900

    맵 이미지 바꾸고 다시 불러오기

--- background.py
+++ background.py
@@ -1,11 +1,18 @@
 from pico2d import load_image
 
+CANVAS_WIDTH  = 1536
+CANVAS_HEIGHT = 1024
+
 class Background:
     def __init__(self):
-        self.image = load_image('background.png')
+        # self.image = load_image('background.png')
+        self.image = load_image('village.png')
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(512, 512)  # 1024x1024 기준 중심
+        self.image.draw(CANVAS_WIDTH // 2,
+                        CANVAS_HEIGHT // 2,
+                        CANVAS_WIDTH,
+                        CANVAS_HEIGHT)
