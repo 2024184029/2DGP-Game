bda3287c055ccbb664aebfe905135947beac5d47
정유리 <2024184029@tukorea.ac.kr>
2025-12-08 14:34:01 +0900

    타이틀모드와 플레이모드 사이에 튜토리얼 모드 추가

--- tutorial_mode.py
+++ tutorial_mode.py
@@ -1,7 +1,6 @@
 from pico2d import *
 import game_framework
-import play_mode
-import tutorial_mode
+import play_mode   # 튜토리얼 끝나면 여기로 넘어감
 
 image = None
 font = None
@@ -15,43 +14,41 @@ def resume():
 
 def init():
     global image, font, intro_bgm
-    image = load_image('title.png')
+    image = load_image('tutorial.png')
     font = load_font('D2Coding.ttc', 15)
 
     intro_bgm = load_music('intro.ogg')
     intro_bgm.set_volume(40)
     intro_bgm.repeat_play()
 
-
 def finish():
-    global image
+    global image, intro_bgm
     if image is not None:
-        del image # 메모리 소멸
+        del image
 
-def exit():
-    global intro_bgm
     if intro_bgm:
         intro_bgm.stop()
+        intro_bgm = None
 
 def update():
     pass
 
 def draw():
     clear_canvas()
-    image.draw(750, 500) # 1500, 1000
-    font.draw(400, 830, "PRESS SPACE OR CLICK TO START", (255, 255, 255))
+    image.draw(750, 500)  # 튜토리얼 이미지 중앙
+    font.draw(400, 100, "PRESS SPACE OR CLICK TO START GAME", (255, 255, 255))
     update_canvas()
 
 def handle_events():
-    # flush events
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-            game_framework.change_mode(tutorial_mode)
-        # 마우스 왼쪽 클릭해도 전환
+            game_framework.change_mode(play_mode)
+
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            game_framework.change_mode(tutorial_mode)
+            game_framework.change_mode(play_mode)
